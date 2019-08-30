4e5cf5fd154d96f6f900af6f5f357faf7ca2537e
Felipe Homrich Melchior <fehmel@gmail.com>
2019-08-29 23:39:22 -0300

    feat: add repositories

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path="/" exact component={Main} />
         <Route path="/repository" component={Repository} />
       </Switch>
     </BrowserRouter>
